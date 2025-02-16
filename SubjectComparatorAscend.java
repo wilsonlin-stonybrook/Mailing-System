5ed97a9d55ae568bff77b3610efb22c1a05ef003
wilsonlin-stonybrook <wilson.lin.2@stonybrook.edu>
2025-02-16 18:17:37 -0500

    Update SubjectComparatorAscend.java

--- SubjectComparatorAscend.java
+++ SubjectComparatorAscend.java
@@ -1,10 +1,4 @@
-/**
- * Wilson Lin
- * 115091711
- * wilson.lin.2@stonybrook.edu
- * Hw 5
- * CSE214.R04 Summer 2024
- */
+
 import java.util.Comparator;
 /**
  * The <code>SubjectComparatorAscend</code> class implements comparator
